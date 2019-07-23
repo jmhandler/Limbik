1b0a336822740213e2b205ce1e06ae10ef9134a0
Josh Handler <handjosh@umich.edu>
2019-07-23 17:28:31 -0400

    Initial Commit

---
+++ limbik/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  limbik
+//
+//  Created by Josh Handler on 7/23/19.
+//  Copyright © 2019 Josh Handler. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
